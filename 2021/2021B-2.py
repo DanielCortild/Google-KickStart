f83e7f51916339932f0acfdffa10f95b4ebf4bce
Daniel Cortild <daniel.cortild@bsc.es>
2021-08-01 23:01:15 +0200

    2021B-2

--- 2021/2021B-2.py
+++ 2021/2021B-2.py
@@ -8,26 +8,38 @@ for T in range(int(input())):
   A = list(map(int, input().split()))
 
   D = [A[i]-A[i-1] for i in range(1, N)]
-  m = 0
-  for i in range(len(D)-1):
-    l = 1
-    joker = 1
-    skip = 0
-    for j in range(i+1, len(D)):
-      if skip:
-        skip = 0
-        continue
-      if D[j] == D[i]:
-        l += 1
-      elif j != len(D)-1 and D[j]+D[j+1] == 2*D[i]:
-        if joker == 1:
-          joker = 0
-          l += 2
-          skip = 1
-        else:
-          break
-      elif j == len(D)-1 and joker == 1:
-        l += 1
-    m = max(l, m)
+  d = {}
 
-  print(f"Case #{T+1}: {m+1}")
+  for i in range(len(D)):
+    if i > 0 and D[i] == D[first]:
+      d[first] += 1
+    else:
+      first = i
+      d[first] = 1
+
+  max_length = 0
+  for k in d.keys():
+    length = d[k]
+    mlen = length
+
+    if k + length < len(D): # Can add at least one at end
+      mlen = length + 1
+    if k > 0: # Can add at least one at start
+      mlen = length + 1
+
+    if k > 1 and D[k - 2] + D[k - 1] == 2 * D[k]: # Change two before
+      mlen = length + 2
+
+    try:
+      if D[k + length] + D[k + length + 1] == 2 * D[k]: # Change two afterwards
+        mlen = length + 2
+        if k + length + 2 in d.keys() and D[k + length + 2] == D[k]: # Can append to the one afterwards
+          mlen += d[k + length + 2]
+    except:
+      pass
+   
+    if mlen > max_length:
+      max_length = mlen
+      mi = k
+
+  print(f"Case #{T+1}: {max_length + 1}")
